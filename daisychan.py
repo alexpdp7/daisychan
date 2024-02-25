116ac4bbc9365738d18a0ef51c412d08eee3eb2b
César Córcoles <ccorcoles@uoc.edu>
2024-02-25 15:59:48 +0100

    Many changes by my brother

--- daisychan.py
+++ daisychan.py
@@ -1,29 +1,41 @@
 import pathlib
+import re
+import shutil
+from tkinter.messagebox import showinfo, showerror
 from tkinter.filedialog import askdirectory
 import xml.etree.ElementTree as ET
 import zipfile
 
 
 def main():
-    path = pathlib.Path(askdirectory(mustexist=True))
-    out = path / "output"
-    out.mkdir()
-    vr = out / "$VROtherBooks"
-    vr.mkdir()
-    ar = out / "archivo"
-    ar.mkdir()
-    for z in path.glob("*.zip"):
-        with zipfile.ZipFile(z) as lz:
-            lz.extractall(out)
-        title, author = parse(z)
-        with zipfile.ZipFile(z) as lz:
-            lz.extractall(vr / title)
-        with zipfile.ZipFile(z) as lz:
-            au = ar / author
-            au.mkdir(exist_ok=True)
-            lz.extractall(au / title)
-        print(title, author)
-
+    try:
+        path = pathlib.Path(askdirectory(mustexist=True))
+        out = path / "output"
+        out.mkdir()
+        vr = out / "$VROtherBooks"
+        vr.mkdir()
+        ar = out / "archivo"
+        ar.mkdir()
+        for z in path.glob("*.zip"):
+            with zipfile.ZipFile(z) as lz:
+                lz.extractall(out)
+            title, author = parse(z)
+            with zipfile.ZipFile(z) as lz:
+                vrb = vr / to_filename(title)
+                lz.extractall(vrb)
+                vrbd = list(vrb.glob("*"))[0]
+                for vrbf in vrb.glob("*/*"):
+                    vrbf.rename(vrb / vrbf.name)
+                vrbd.rmdir()
+            with zipfile.ZipFile(z) as lz:
+                au = ar / to_filename_preserve_comma(author)
+                au.mkdir(exist_ok=True)
+                shutil.copy(z, au / (to_filename(title) + ".zip"))
+            print(title, author)
+        showinfo("Finished")
+    except Exception as e:
+        showerror(str(e))
+        raise e
 
 def parse(path: pathlib.Path):
     with zipfile.ZipFile(path) as z:
@@ -34,6 +46,14 @@ def parse(path: pathlib.Path):
         title = meta["dc:title"]
         author = meta["dc:creator"]
         return title, author
+
+
+def to_filename(s):
+    return re.sub(r"\W+", " ", s).strip()
+
+
+def to_filename_preserve_comma(s):
+    return re.sub(r"[^\w,]+", " ", s).strip()
 
 
 
